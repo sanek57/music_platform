96bb81829e7cf7d1600ecdaf4d5d21a0c843ca2f
sanek57 <sanekofblack@gmail.com>
2025-10-06 17:09:31 +1000

    подключил схемы в модули

--- server/src/album/album.module.ts
+++ server/src/album/album.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AlbumController } from './album.controller';
 import { AlbumService } from './album.service';
+import { Album, AlbumSchema } from './schemas/album.schema';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+  // подключаем схемы в модуль
+  imports: [
+    MongooseModule.forFeature([{ name: Album.name, schema: AlbumSchema }]),
+  ],
   controllers: [AlbumController],
   providers: [AlbumService],
 })
